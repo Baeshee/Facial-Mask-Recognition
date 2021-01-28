bd0ae0ffca99bbc94395287467bf6cd006e4d1d4
Baeshee <basbarg@gmail.com>
2021-01-28 17:21:01 +0100

    Clean up

--- Email.py
+++ Email.py
@@ -41,24 +41,3 @@ def sendMail(foto):
         server.ehlo()
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, text)
-
-
-# import smtplib, ssl
-#
-# port = 587  # For starttls
-# smtp_server = "smtp.gmail.com"
-#sender_email = "testingpythonsendemail@gmail.com"
-#receiver_email = ""
-# password = "GuitarGuy997"
-# message = """\
-# Subject: Hi there
-#
-# This message is sent from Python."""
-#
-# context = ssl.create_default_context()
-# with smtplib.SMTP(smtp_server, port) as server:
-#     server.ehlo()  # Can be omitted
-#     server.starttls(context=context)
-#     server.ehlo()  # Can be omitted
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, receiver_email, message)
