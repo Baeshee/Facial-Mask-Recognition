9300f4d05565559df3f5579c77acbb5f38dc8e12
edwin <s1115513@student.hsleiden.nl>
2021-01-30 01:41:37 +0100

    Eerste opzet volledige threaded run; Optie om mails in bestand te zetten, alleen pass eenmalig invoeren; Bij elke foldercheck wordt iedere nieuwe non-maskfoto afzonderlijk gemaild;

--- Email.py
+++ Email.py
@@ -1,16 +1,19 @@
-import email, smtplib, ssl
-
+import getpass
+import smtplib
+import ssl
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+sender_email = ""
+password = ""
+receiver_email = ""
+
+
 def sendMail(foto):
     port = 587
     smtp_server = "smtp.gmail.com"
-    password = input("Enter your Sender (GMAIL) Password") #hier komt je wachtwoord
-    sender_email = input("Enter your Sender (GMAIL) Email: ") #hier komt je email adress
-    receiver_email = input("Enter the Receiver Email: ") #hier komt de receiver email adress
     subject = "Person without mouth mask"
     body = "The following person is walking around the complex without a mouth mask, I would kindly ask the floor patrol to approach this individual and ask them to wear one or leave the complex."
 
@@ -29,7 +32,7 @@ def sendMail(foto):
 
     encoders.encode_base64(part)
 
-    part.add_header("Content-Disposition", f"attachment; filename={filename}",)
+    part.add_header("Content-Disposition", f"attachment; filename={filename}", )
 
     message.attach(part)
     text = message.as_string()
@@ -41,3 +44,28 @@ def sendMail(foto):
         server.ehlo()
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, text)
+
+
+def setEmailData():
+    global sender_email
+    global receiver_email
+
+    # Set your 2 email addresses in this file to prevent entering them each time.
+    emails = open('EmailSettings.txt').readlines()
+    if len(emails) > 2:
+        if "@gmail.com" in emails[1] and "@" in emails[2]:
+            askPassword()
+            sender_email = emails[1]
+            receiver_email = emails[2]
+            return
+
+    sender_email = input("Enter your Sender (GMAIL) Email: ")  # hier komt je email address
+    askPassword()
+    receiver_email = input("Enter the Receiver Email: ")  # hier komt de receiver email address
+
+
+def askPassword():
+    global password
+    # Go to 'Edit Configurations' (Next to RUN button) and then select 'Emulate terminal in output console'.
+    # This must be set in script calling setEmailData() [Main], otherwise it WON'T ask/appear!
+    password = getpass.getpass(prompt="Enter your Sender (GMAIL) Password: ")
--- Watcher.py
+++ Watcher.py
@@ -3,25 +3,22 @@ import time
 import getpass
 from Email import sendMail
 
+
 def getSize(filename):
     st = os.stat(filename)
     return st.st_size
 
-path_to_watch = "C:\\Users\\" + getpass.getuser() + "\\Pictures\\MaskMonitoring"
-before = dict([(f, None) for f in os.listdir(path_to_watch)])
-while 1:
-    time.sleep(10)
-    after = dict([(f, None) for f in os.listdir(path_to_watch)])
-    added = [f for f in after if not f in before]
-    # removed = [f for f in before if not f in after]
-    if added:
-        print("Added: ", ", ".join(added))
-        # foreach picture in added:
-        # checkForMask.
-        # If not WearMask:
-        foto = path_to_watch + "\\" + added[0]
-        if (getSize(foto) > 100000):
-            sendMail(foto)
-    # if removed:
-    #     print("Removed: ", ", ".join(removed))
-    before = after
+
+def startWatchingNoMaskFolder():
+    path_to_watch = "C:/Users/" + getpass.getuser() + "/Pictures/MaskMonitoring/ZonderMasker"
+    before = dict([(f, None) for f in os.listdir(path_to_watch)])
+    while 1:
+        time.sleep(5)
+        after = dict([(f, None) for f in os.listdir(path_to_watch)])
+        added = [f for f in after if not f in before]
+        if added:
+            print("Added: ", ", ".join(added))
+            for file in added:
+                foto = path_to_watch + "/" + file
+                sendMail(foto)
+        before = after
